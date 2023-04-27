be458108e274b5177085eef34623fbf90ef1965d
A.chitrarasu <chitrarasumit@gmail.com>
2023-04-27 11:36:43 +0530

    cors updated

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 // const cors=require("cors");
 const router=require("./routes/router")
 const express=require("express");
+const cors=require("cors")
 const app=express();
 // const cors=require("cors");
 //middleware
 app.use(express.json());
+app.use(cors());
 app.use(router)
 
 
